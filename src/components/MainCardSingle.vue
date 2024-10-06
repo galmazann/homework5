4a36f72437e0ff051b83f8959af938525abf88ce
Gonzalo Almazan <galmazan@stu.southtexascollege.edu>
2024-10-06 16:14:48 -0500

    Card data and style done

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,8 +1,21 @@
 <script setup>
 import { faker } from '@faker-js/faker'
 const fullname = faker.person.fullName()
- </script>
+const firstname = faker.person.firstName()
+const lastname = faker.person.lastName()
+const job_title = faker.person.jobTitle()
+const bio = faker.lorem.lines (2)
+const username = faker.internet.userName({firstName: firstname, lastName:lastname}).toLowerCase()
+
+</script>
 
 <template>
-{{ fullname}} <br />
- </template>
+    <!-- {{ fullname }} <br /> -->
+    <div class="border border-gray-400 rounded-1g shadow bg-gray-200 h-full"> 
+            <img :src="faker.image.urlLoremFlickr({category: 'cars'})">
+            <div class="p-4">
+                <p class="mb-2 text-2x1 font-bold">{{ fullname }}</p>
+                <p class="mb-3 font-normal text-gray-700"> {{job_title}}</p> 
+                <p class="mb-3 font-normal ☐text-gray-700"> {{ bio }}</p> </div>
+    </div> 
+</template>
