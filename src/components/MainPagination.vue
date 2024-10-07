37849b853951fc271ff26624bbe0103ae2fc7040
Gonzalo Almazan <galmazan@stu.southtexascollege.edu>
2024-10-06 22:38:17 -0500

    added style to pagination

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -1,11 +1,12 @@
 <template>
-<div>
 
-    <button @click="prevPage" :disabled="activePage === 1">PREV</button>
+<div class="flex justify-center gap-4 p-8">
 
-    <button v-for="page in pages" :key="page" class=""> {{ page }}</button>
+    <button @click="prevPage" :disabled="activePage === 1" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md">PREV</button>
+    
+    <button v-for="page in pages" key="page" class="rounded-md ■bg-slate-100 p-2 font-medium text-slate-780 shadow-md" :class="page === activePage? 'bg-yellow-700 text-slate-100': '' "> {{ page }}</button>
 
-    <button @click="nextPage" :disabled="activePage === pages">NEXT</button>
+    <button @click="nextPage" :disabled="activePage === pages" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md">NEXT</button>
 
 </div>
 
